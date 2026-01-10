9dfaab2d1aa1cec79082659819a2426bdb4fe0c1
katiak <katiakweb@users.noreply.github.com>
2026-01-10 19:28:29 -0300

    Remove Matomo tracking code from Header.tsx
    Removed Matomo tracking script from Header component.

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -48,21 +48,6 @@ const Header = () => {
           </Button>
         </div>
       </div>
-      <!-- Matomo -->
-<script>
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://matomo.oguru.dev/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '4']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Code -->
     </header>
   );
 };
